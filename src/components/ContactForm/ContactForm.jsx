914c93a5f0445f50688e28502cb2808565ad305e
Ola Połaniecka <97olka@gmail.com>
2024-02-01 12:38:33 +0100

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,35 +1,26 @@
 import { useState } from 'react';
 import { nanoid } from 'nanoid';
 
-const ContactForm = () => {
+const ContactForm = ({ onAdd, onCheckUnique }) => {
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
 
-  const handleChangeForm = e => {
-    const name = e.target.name;
-    const value = e.target.value;
-    const number = e.target.number;
-    setName(prev => {
-      return { ...prev, [name]: value };
-    });
-    setNumber(prev => {
-      return { ...prev, [number]: value };
-    });
+  const handleChangeFormName = e => {
+    setName(e.target.value);
+  };
+
+  const handleChangeFormNumber = e => {
+    setNumber(e.target.value);
   };
 
   const handleFormSubmit = event => {
     event.preventDefault();
-    const { name, number } = this.state;
-    const { onAdd } = this.props;
-
     const inValidateForm = validateForm();
     if (!inValidateForm) return;
     onAdd({ id: nanoid(), name, number });
   };
 
   const validateForm = () => {
-    const { name, number } = this.state;
-    const { onCheckUnique } = this.props;
     if (!name || !number) {
       alert('Some field is empty');
       return false;
@@ -45,7 +36,7 @@ const ContactForm = () => {
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         value={name}
-        onChange={handleChangeForm}
+        onChange={handleChangeFormName}
         required
       />
       <input
@@ -54,7 +45,7 @@ const ContactForm = () => {
         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         value={number}
-        onChange={handleChangeForm}
+        onChange={handleChangeFormNumber}
         required
       />
 
